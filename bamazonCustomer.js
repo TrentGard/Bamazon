6ad813346aa1faba30cf64bbbbdb5ea99c998b36
Trent Gard <trentgard@trents-mbp.attlocal.net>
2017-11-19 19:43:33 -0600

    updated homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ var inquirer = require('inquirer');
 
 var mysql = require('mysql');
 
+require('console.table');
+
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
@@ -11,40 +13,102 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(error){
-	if (error) throw error;
-	connection.query("SELECT item_id, product_name, price FROM products", function(error, result, fields){
+	if (error) {
+		console.error("Error connecting: " + error.stack);
+	}
+	loadProducts();
+});
+
+function loadProducts() {
+
+	connection.query("SELECT * FROM products", function(error, result) {
 		if (error) throw error;
-		console.log(result);
-	connection.end();
+
+		console.table(result);
+		promptUserItem(result);
 	});
 
-function promptUserInput () {
-
-	inquirer.prompt([
-		name: 'ID'.
-		type: 'list',
-		message: 'Please select the item_id of the product you would like to purchase!',
-		choices: function(){
-			var userInputArray = [];
-			for (var i = 0; i < result.length; i++) {
-				userInputArray.push(result[i].item_id);
-			}
-			return userInputArray;
-		},
-		message: "How much would you like to bid?"
-		},
-		]).then(function() {
-			if (userInput != item_id) {
-				console.log("Invalid input!");
-				return;
-			}else{
-				function(answer){
-					var chosenID;
-					//determine if userInput > price, if not, console.log("bid too low!") and return
-
-				}
-			}
-		})
 }
 
-});
+function promptUserItem (inventory) {
+
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "choice",
+				message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+				validate: function(value) {
+					return !isNaN(value) || value.toLowerCase() === "q";
+					console.log("Invalid input!");
+				}
+			}
+		])
+		.then(function(value) {
+			checkIfShouldExit(value.choice);
+			var choiceId = parseInt(value.choice);
+			var product = checkInventory(choiceId, inventory);
+
+			if (product) {
+				promptUserQuantity(product);
+			}
+			else {
+				console.log("\nThat item is not in the inventory.");
+				loadProducts();
+			}
+		});
+}
+
+function promptUserQuantity(product) {
+	inquirer
+		.prompt([
+		{
+			type: "input",
+			name: "quantity",
+			message: "How many would you like? [Quit with Q]",
+			validate: function(value) {
+				return value > 0 || value.toLowerCase() === "q";
+			}
+		}
+		])
+		.then(function(value) {
+			checkIfShouldExit(value.quantity);
+			var quantity = parseInt(value.quantity);
+
+			if(quantity > product.stock_quantity) {
+				console.log("\nInsufficient quantity!");
+				loadProducts();
+			}
+			else {
+				makePurchase(product, quantity);
+			}
+		});
+}
+
+function makePurchase(product, quantity) {
+	connection.query(
+		"UPDATE products SET stock_quantity - ? WHERE item_id = ?",
+		[quantity, product.item_id],
+		function(err, res) {
+			console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "!");
+		loadProducts();
+	}
+	);
+}
+
+function checkInventory (choiceId, inventory) {
+	for (var i = 0; i <inventory.length; i++) {
+		if (inventory[i].item_id === choiceId) {
+			return inventory[i];
+		}
+	}
+
+	return null;
+}
+
+function checkIfShouldExit(choice) {
+	if (choice.toLowerCase() === "q") {
+		console.log("Goodbye!");
+		process.exit(0);
+	}
+}
